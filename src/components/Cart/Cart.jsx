3846a17d84532c9880bc15ba4fe47a66298b8366
bhuiamhe <bhuiamh@gmail.com>
2023-04-05 14:17:18 +0600

    Cart show by LocalStoragr

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,19 +4,26 @@ import './Cart.css'
 const Cart = ({ cart }) => {
 
 
+
     let total = 0;
+    let quantity = 0;
+
 
 
     for (const product of cart) {
+        // product.quantity = product.quantity || 1; This done by alternative way
         total = total + parseFloat(product.price);
-
+        quantity = quantity + product.quantity;
     }
+
     let shippingCost = Number((total * 15) / 100).toFixed(2);
     let tax = Number((total * 10) / 100).toFixed(2);
+
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: $ {total}</p>
             <p>Total Shipping: {shippingCost} <span>(15%)</span></p>
             <p>Tax: {tax} </p>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,16 +6,26 @@ import Swal from 'sweetalert2'
 
 
 const Product = (props) => {
-    const { api_featured_image, brand, category, description, name, price, website_link } = props.product;
+    const { img, brand, category, description, name, price, website_link } = props.product;
 
     const handelAddToCart = props.handelAddToCart
 
     const noPrice = () => price == 0;
 
+    const addToCart = () => {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Product successfully added to your cart!',
+            showConfirmButton: false,
+            timer: 1500,
+        })
+    }
+
 
     return (
         <div className='product'>
-            <img src={api_featured_image} ></img>
+            <img src={img} ></img>
             <div className='product-info'>
                 <p className='product-name'>{name}</p>
                 <p className='product-category'>{category}</p>
@@ -34,6 +44,7 @@ const Product = (props) => {
                 }
                 else {
                     handelAddToCart(props.product);
+                    addToCart();
                 }
             }} className='btn-cart'>
                 Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
